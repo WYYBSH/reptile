3f195aef74141ba0c961b57a8e791d591023ce80
tsubame <1534995542@qq.com>
2018-11-22 18:57:38 +0800

    rewrite url

--- url.py
+++ url.py
@@ -1,24 +1,25 @@
-import urllib2
+import urllib.request
+import http.cookiejar
 
 url = "https://www.baidu.com/"
 # 简单爬虫
 print('method one')
-response1 = urllib2.urlopen(url)
+response1 = urllib.request.urlopen(url)
 print(response1.getcode())
 print(len(response1.read()))
 
 print('method two')
-request = urllib2.Request(url)
-request.add_header("user-agent","Mozilla/5.0")
-response2 = urllib2.urlopen(request)
+request = urllib.request.Request(url)
+request.add_header("user-agent", "Mozilla/5.0")
+response2 = urllib.request.urlopen(request)
 print(response2.getcode())
 print(len(response2.read()))
 
 print('method three')
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
-response3 = urllib2.urlopen(url)
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+urllib.request.install_opener(opener)
+response3 = urllib.request.urlopen(url)
 print(response3.getcode())
 print(cj)
 print(response3.read())
